39aa27d1f416bc33fc2def8a2a0d619232271046
Shivam Sharma <shivxmsharma@gmail.com>
2026-03-13 16:11:23 +0530

    feat(theme): add doc-kit.config.mjs with webpack theming

---
+++ doc-kit.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  global: {
+    repository: "webpack/webpack",
+  },
+  web: {
+    title: "webpack",
+    imports: {
+      "#config/Logo": "./plugins/theme/ui/WebpackLogo.jsx",
+    },
+  },
+};
