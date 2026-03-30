919f5d53f74425822124f9c970d88380383f5dce
Rahul Kumar Rai <rahulkumarrai2711@gmail.com>
2026-03-30 20:36:21 +0530

    remove error handle as per suggestation

--- clone-webpack.mjs
+++ clone-webpack.mjs
@@ -21,33 +21,28 @@ if (!ref) {
 
 console.log(`Cloning webpack repository at ref: ${ref}...`);
 
-try {
-  if (fs.existsSync(webpackDir)) {
-    console.log(
-      `Repository already exists at ${webpackDir}. Fetching latest updates...`
-    );
-    execFileSync('git', ['fetch', '--all'], {
-      cwd: webpackDir,
+if (fs.existsSync(webpackDir)) {
+  console.log(
+    `Repository already exists at ${webpackDir}. Fetching latest updates...`
+  );
+  execFileSync('git', ['fetch', '--all'], {
+    cwd: webpackDir,
+    stdio: 'inherit',
+  });
+} else {
+  // Clone the repository
+  console.log('Running git clone https://github.com/webpack/webpack.git...');
+  execFileSync(
+    'git',
+    ['clone', 'https://github.com/webpack/webpack.git', webpackDir],
+    {
       stdio: 'inherit',
-    });
-  } else {
-    // Clone the repository
-    console.log('Running git clone https://github.com/webpack/webpack.git...');
-    execFileSync(
-      'git',
-      ['clone', 'https://github.com/webpack/webpack.git', webpackDir],
-      {
-        stdio: 'inherit',
-      }
-    );
-  }
-
-  // Checkout the target commit
-  console.log(`Checking out commit/branch: ${ref}...`);
-  execFileSync('git', ['checkout', ref], { cwd: webpackDir, stdio: 'inherit' });
-
-  console.log('Successfully completed clone-webpack script.');
-} catch (error) {
-  console.error('Error during git clone or checkout:', error.message);
-  process.exit(1);
+    }
+  );
 }
+
+// Checkout the target commit
+console.log(`Checking out commit/branch: ${ref}...`);
+execFileSync('git', ['checkout', ref], { cwd: webpackDir, stdio: 'inherit' });
+
+console.log('Successfully completed clone-webpack script.');
