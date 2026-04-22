2a3781845ebb2302bcacc2e9a7fa64377415d831
Aviv Keller <me@aviv.sh>
2026-04-22 09:46:22 -0400

    chore(vercel)

--- scripts/clone.mjs
+++ scripts/clone.mjs
@@ -2,30 +2,33 @@ import { readFile, access } from 'node:fs/promises';
 import { execFileSync } from 'node:child_process';
 
 const WEBPACK_DIR = 'webpack';
+const WEBPACK_REPO = 'https://github.com/webpack/webpack.git';
 const REF = (await readFile('HEAD_COMMIT', 'utf-8')).trim();
 
-try {
-  await access(WEBPACK_DIR);
-  execFileSync('git', ['fetch', '--all'], {
-    cwd: WEBPACK_DIR,
+const git = (args, options = {}) =>
+  execFileSync('git', args, {
     stdio: 'inherit',
+    ...options,
   });
-} catch {
-  execFileSync(
-    'git',
-    ['clone', 'https://github.com/webpack/webpack.git', WEBPACK_DIR],
-    {
-      stdio: 'inherit',
-    }
-  );
+
+let hasGitRepo = false;
+
+try {
+  await access(`${WEBPACK_DIR}/.git`);
+  hasGitRepo = true;
+} catch {}
+
+if (!hasGitRepo) {
+  git(['clone', WEBPACK_REPO, WEBPACK_DIR]);
+} else {
+  try {
+    git(['remote', 'get-url', 'origin'], { cwd: WEBPACK_DIR, stdio: 'ignore' });
+  } catch {
+    git(['remote', 'add', 'origin', WEBPACK_REPO], { cwd: WEBPACK_DIR });
+  }
+
+  git(['fetch', 'origin', '--tags', '--prune'], { cwd: WEBPACK_DIR });
 }
 
-execFileSync('git', ['fetch', 'origin', REF], {
-  cwd: WEBPACK_DIR,
-  stdio: 'inherit',
-});
-
-execFileSync('git', ['checkout', REF], {
-  cwd: WEBPACK_DIR,
-  stdio: 'inherit',
-});
+git(['fetch', 'origin', REF], { cwd: WEBPACK_DIR });
+git(['checkout', '--detach', REF], { cwd: WEBPACK_DIR });
