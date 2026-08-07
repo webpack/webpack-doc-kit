af7dce9ca96dbbfeb57ffcc6de68f619a127a380
Mohamed Shams El-Deen <mohamedshamseldeen95@gmail.com>
2026-08-07 23:41:12 +0300

    test(theme): generate  input.d.ts file

--- tests/theme/theme.test.mjs
+++ tests/theme/theme.test.mjs
@@ -1,21 +1,27 @@
 import { test } from 'node:test';
 import { Application } from 'typedoc';
-import { readdirSync, readFileSync, rmSync } from 'node:fs';
+import { readdirSync, readFileSync, rmSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path/posix';
+import { execSync } from 'node:child_process';
 import { typeDocConfig } from '../../scripts/markdown/api/utils.mjs';
 
 const fixturesDir = './tests/theme/fixtures';
 const outputDir = './tests/theme/.temp';
+const typesDir = './tests/theme/.temp/types';
+const tsconfig = { include: ['**/*.d.ts'] };
 
 test('TypeDoc Theme - Edge Cases Fixture', async t => {
-  const inputFilePath = join(fixturesDir, 'input.js');
+  execSync(`npx tsc -p ${join(fixturesDir, 'tsconfig.json')}`, {
+    stdio: 'inherit',
+  });
+  writeFileSync(join(typesDir, 'tsconfig.json'), JSON.stringify(tsconfig));
 
   const app = await Application.bootstrapWithPlugins({
     ...typeDocConfig,
-    entryPoints: [inputFilePath],
+    entryPoints: [join(typesDir, 'input.d.ts')],
     out: outputDir,
     publicPath: '/',
-    tsconfig: join(fixturesDir, 'tsconfig.json'),
+    tsconfig: join(typesDir, 'tsconfig.json'),
   });
 
   const project = await app.convert();
